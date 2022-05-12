e1f15996258df38261854be682069f06c20853c6
Alexander <alex@deekerno.com>
2022-05-11 22:27:14 -0400

    Fix margins on transaction chart (#76)

--- src/components/Charts/TransactionVolume.tsx
+++ src/components/Charts/TransactionVolume.tsx
@@ -8,7 +8,16 @@ type Props = {
 }
 
 export default function TransactionVolume({ data }: Props) {
-  const yAccessor = React.useCallback((d: Metric) => d.transactions_count, [])
+  const yAccessor = React.useCallback((d: Metric) => d.transactions_count / 1e3, [])
 
-  return <GeneralChart data={data} marginLeft={50} yAccessor={yAccessor} />
+  return (
+    <GeneralChart
+      data={data}
+      marginLeft={50}
+      yAccessor={yAccessor}
+      leftAxisFormatter={function format(d) {
+        return `${d}K`
+      }}
+    />
+  )
 }
